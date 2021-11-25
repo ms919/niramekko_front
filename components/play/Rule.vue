dfd312d5e625573f943a4cf903c29508eba64c69
ms919 <sakuraimiki919@gmail.com>
2021-11-25 21:14:07 +0900

    Add: mode別ルール処理

--- components/play/Rule.vue
+++ components/play/Rule.vue
@@ -2,29 +2,44 @@
 	<div class="text-center section-wrapper">
 		<h3>ルール説明・注意点</h3>
 		<div class="text-wrapper">
-			<p>
-				texttexttexttexttexttext<br />texttexttexttexttexttexttext
-			</p>
-			<p>
-				texttexttexttexttexttext<br />texttexttexttexttexttexttext
-			</p>
-			<p>
-				texttexttexttexttexttext<br />texttexttexttexttexttexttext
-			</p>
-			<p>
-				texttexttexttexttexttext<br />texttexttexttexttexttexttext
-			</p>
+			<p v-html="text"></p>
 		</div>
-		<a @click="$store.dispatch('gotoNext')" class="btn sm-button btn-pink">START</a>
+		<a @click="$store.dispatch('gotoNext')" class="btn sm-button btn-pink"
+			>START</a
+		>
 	</div>
 </template>
 
 <script>
+import fixed from "~/const/const.js";
 export default {
-  mounted() {
-    this.$store.dispatch("getItems");
-  }
-}
+	computed: {
+		mode() {
+			return this.$store.getters.mode;
+		},
+		text() {
+			switch (this.mode) {
+				case fixed.MODE.NORMAL:
+					return (
+							"texttexttexttexttexttext<br/>texttexttexttexttexttexttext"
+					);
+				case fixed.MODE.DOJYO:
+					return (
+						"hogehogehogehoge<br/>texttexttexttexttexttexttext"
+					);
+				case fixed.MODE.REVENGE:
+					return (
+						"fugafugafugafuga<br/>texttexttexttexttexttexttext"
+					);
+				default:
+					""
+			}
+		},
+	},
+	mounted() {
+		this.$store.dispatch("getItems");
+	},
+};
 </script>
 
 <style scoped>
--- pages/modeSelect.vue
+++ pages/modeSelect.vue
@@ -1,24 +1,45 @@
 <template>
-  <div class="text-center display-col justify-center wrapper-height">
-    <div>
-      <a @click="$store.dispatch('setMode', normal)" class="btn lg-button btn-pink">normal mode</a>
-      <p>5つの動画を見て最終スコアを競うモード</p>
-    </div>
-    <div>
-      <a @click="$store.dispatch('setMode', dojyo)" class="btn lg-button btn-pink">道場破りmode</a>
-      <p>最新ベスト5に挑むモード</p>
-    </div>
-  </div>
+	<div class="text-center display-col justify-center wrapper-height">
+		<div>
+			<a
+				@click="$store.dispatch('setMode', normal)"
+				class="btn lg-button btn-pink"
+				>normal mode</a
+			>
+			<p>5つの動画を見て最終スコアを競うモード</p>
+		</div>
+		<div>
+			<a
+				@click="$store.dispatch('setMode', dojyo)"
+				class="btn lg-button btn-pink"
+				>道場破りmode</a
+			>
+			<p>最新ベスト5に挑むモード</p>
+		</div>
+		<div v-if="loginFlg">
+			<a
+				@click="$store.dispatch('setMode', revenge)"
+				class="btn lg-button btn-pink"
+				>リベンジmode</a
+			>
+			<p>過去に笑ってしまった動画にリベンジするモード</p>
+		</div>
+	</div>
 </template>
 
 <script>
 import fixed from "~/const/const.js";
 export default {
-  mounted(){
-    this.normal = fixed.MODE.NORMAL;
-    this.dojyo = fixed.MODE.DOJYO;
-    this.revenge = fixed.MODE.REVENGE;
-    this.$store.dispatch("clearItem");
-  }
-}
+	computed: {
+		loginFlg() {
+			return this.$store.getters["session/loginFlg"];
+		},
+	},
+	mounted() {
+		this.normal = fixed.MODE.NORMAL;
+		this.dojyo = fixed.MODE.DOJYO;
+		this.revenge = fixed.MODE.REVENGE;
+		this.$store.dispatch("clearItem");
+	},
+};
 </script>
