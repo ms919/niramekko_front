bbf3070d19a9adc0e3f57dd18b396fbca2cf5250
ms919 <sakuraimiki919@gmail.com>
2021-11-25 15:06:49 +0900

    Add: ログイン後メニュー追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,12 +5,17 @@
 				>niramekko</NuxtLink
 			>
 			<nav>
-				<a
-					v-if="loginFlg"
-					@click="$store.dispatch('session/logout')"
-					class="white-shadow"
-					>logout</a
-				>
+				<div v-if="loginFlg" ref="elRoot">
+					<a @click="menuFlg = !menuFlg">
+						<img :src="`${userImg}`" class="img-circle" />
+					</a>
+					<ul class="menu" v-show="menuFlg">
+						<li><NuxtLink to="/user">profile</NuxtLink></li>
+						<li><a @click="$store.dispatch('session/logout')">logout</a></li>
+						<li><NuxtLink to="/modeSelect">play</NuxtLink></li>
+						<li><NuxtLink to="/video">share video</NuxtLink></li>
+					</ul>
+				</div>
 				<NuxtLink v-else to="/login" class="white-shadow">login</NuxtLink>
 			</nav>
 		</header>
@@ -19,21 +24,52 @@
 	</div>
 </template>
 <script>
+import { mapGetters } from "vuex";
 export default {
-	computed: {
-		loginFlg() {
-			return this.$store.getters["session/loginFlg"];
-		},
+	data() {
+		return {
+			menuFlg: false,
+		};
 	},
-	// methods:{
-	// 	logout(){
-	// 		this.$axios
-	// 			.get("api/v1/logout")
-	// 			.then(() => {
-	// 				this.$store.dispatch("session/removeLoginFlg");
-	// 			})
-	// 			.catch((e) => console.log(e));
-	// 	}
-	// }
+	computed: {
+		...mapGetters({
+			loginFlg: "session/loginFlg",
+			userImg: "session/userImg",
+		}),
+	},
+	mounted() {
+		window.addEventListener(
+			"click",
+			(this._onBlurHandler = (event) => {
+				if (!this.$refs.elRoot.contains(event.target)) {
+					this.menuFlg = false;
+				}
+			})
+		);
+	},
+	beforeDestroy() {
+		window.removeEventListener("click", this._onBlurHandler);
+	},
 };
 </script>
+<style>
+.menu {
+	padding: 1rem;
+	background-color: #ffffff;
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+	margin-left: auto;
+	position: absolute;
+	right: 1rem;
+	border-radius: 10px;
+}
+li {
+	margin: 0.5rem 0;
+}
+li a {
+	display: inline-block;
+	width: 100%;
+}
+li a:hover {
+	color: #e43965;
+}
+</style>
