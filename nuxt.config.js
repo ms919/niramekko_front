4363ba7e0b0b516ff3811cb6accdea89ba05685a
ms919 <sakuraimiki919@gmail.com>
2021-12-13 18:45:48 +0900

    Add: OGP画像 #7

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,35 @@ export default {
 		title: "niramekko",
 		htmlAttrs: {
 			lang: "ja",
+			prefix: "og: http://ogp.me/ns#",
 		},
 		meta: [
 			{ charset: "utf-8" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
-			{ hid: "description", name: "description", content: "" },
-			{ name: "format-detection", content: "telephone=no" },
+			{ hid: "description", name: "description", content: "social niramekko game" },
+			{ name: "format-detection", content: "telephone=no,address=no,email=no" },
+			{ hid: "og:site_name", property: "og:site_name", content: "niramekko" },
+			{ hid: "og:type", property: "og:type", content: "website" },
+			{
+				hid: "og:url",
+				property: "og:url",
+				content: "https://niramekko.herokuapp.com",
+			},
+			{ hid: "og:title", property: "og:title", content: "niramekko" },
+			{
+				hid: "og:description",
+				property: "og:description",
+				content: "social niramekko game",
+			},
+			{
+				hid: "og:image",
+				property: "og:image",
+				content: "https://niramekko.herokuapp.com/ogp.png",
+			},
+			{ name: "twitter:card", content: "summary_large_image" },
+			{ name: "twitter:creator", content: "@sakkkkup" },
 		],
-		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }, { rel: "canonical", href: "https://niramekko.herokuapp.com" }],
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
@@ -31,7 +52,7 @@ export default {
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
-		{ src: "~/plugins/flash-message.js", mode: "client" },
+		{ src: "~/plugins/flash-message.js" },
 		{ src: "~/plugins/init.js" },
 	],
 
