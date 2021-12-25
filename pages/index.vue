55743e33b78ead643ca75447eddbd3a224935281
ms919 <sakuraimiki919@gmail.com>
2021-12-25 12:44:07 +0900

    Fix: topページ内容再検討(ひとこと追記) #26

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,19 @@
 <template>
 	<div class="text-center display-col justify-space-around wrapper-height">
 		<div>
-			<h2>笑って、笑わせて</h2>
-			<h1 class="app-name-font">niramekko</h1>
-			<h2>social niramekko game</h2>
+			<div>
+				<h2>笑って、笑わせて</h2>
+				<h1 class="app-name-font">niramekko</h1>
+				<h2>social niramekko game</h2>
+			</div>
+			<p>
+				おもしろ動画とにらめっこ。サクッと気分を切り替えよう。
+			</p>
 		</div>
 		<NuxtLink to="/modeSelect" class="btn lg-button btn-pink">PLAY</NuxtLink>
 	</div>
 </template>
+
 <script>
 export default {
 	mounted(){
@@ -18,3 +24,10 @@ export default {
 	}
 }
 </script>
+
+<style scoped>
+p {
+	font-size: 0.8rem;
+	margin-top: 5rem;
+}
+</style>
