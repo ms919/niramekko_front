65c119471c3c9cab1e9bb98a641a3c5ea262bb6c
ms919 <sakuraimiki919@gmail.com>
2021-12-31 15:04:26 +0900

    Fix: userProfileをgridに移行 #28

--- components/user/Profile.vue
+++ components/user/Profile.vue
@@ -1,72 +1,70 @@
 <template>
-	<div class="display-col justify-center user-wrapper">
-		<div class="user-upper justify-between user-width">
-			<div class="display-row record user-div-style user-div-wrapper">
-				<p class="record-left pink text-center">
-					<span class="score-font">{{ total_score }}</span><br />score
-				</p>
-				<div class="display-col justify-center record-right pink">
-					<div class="record-right-width display-row justify-space-around">
-						<div class="title gold">
-							<fa :icon="faMeh" class="layer-icon" />
-							<p>{{ this.records.gold }}</p>
-						</div>
-						<div class="title iron">
-							<fa :icon="faSmile" class="layer-icon" />
-							<p>{{ this.records.iron }}</p>
-						</div>
-						<div class="title soil">
-							<fa :icon="faSmileBeam" class="layer-icon" />
-							<p>{{ this.records.soil }}</p>
-						</div>
+	<div class="user-wrapper">
+		<div class="display-row record user-div-style">
+			<p class="record-left pink text-center">
+				<span class="score-font">{{ total_score }}</span><br />score
+			</p>
+			<div class="display-col justify-center record-right pink">
+				<div class="record-right-width display-row justify-space-around">
+					<div class="title gold">
+						<fa :icon="faMeh" class="layer-icon" />
+						<p>{{ this.records.gold }}</p>
 					</div>
-					<br />
-					<div class="record-right-width display-row justify-space-around">
-						<div class="title bg-orange">
-							<fa :icon="faLaughBeam" class="layer-icon" />
-							<p>{{ this.records.smile }}</p>
-						</div>
-						<div class="title bg-orange">
-							<fa :icon="faLaughSquint" class="layer-icon" />
-							<p>{{ this.records.laugh }}</p>
-						</div>
+					<div class="title iron">
+						<fa :icon="faSmile" class="layer-icon" />
+						<p>{{ this.records.iron }}</p>
+					</div>
+					<div class="title soil">
+						<fa :icon="faSmileBeam" class="layer-icon" />
+						<p>{{ this.records.soil }}</p>
 					</div>
 				</div>
-			</div>
-			<div class="user-div-wrapper user-right-wrapper">
-				<div class="display-row user-div-style profile">
-					<div>
-						<p class="orange">name</p>
-						{{ this.user.name }}
+				<br />
+				<div class="record-right-width display-row justify-space-around">
+					<div class="title bg-orange">
+						<fa :icon="faLaughBeam" class="layer-icon" />
+						<p>{{ this.records.smile }}</p>
 					</div>
-					<fa
-						:icon="faCog"
-						@click="changeComponent('UserSettings')"
-						class="user-icon cog"
-					/>
-				</div>
-				<div class="user-div-style video display-row justify-between">
-					<p class="orange">video</p>
-					<div class="display-row justify-between video-icons">
-						<fa
-							:icon="faListUl"
-							@click="$store.dispatch('goToVideo', 'Video')"
-							class="user-icon"
-						/>
-						<fa
-							:icon="faPlus"
-							@click="$store.dispatch('goToVideo', 'VideoNew')"
-							class="user-icon"
-						/>
+					<div class="title bg-orange">
+						<fa :icon="faLaughSquint" class="layer-icon" />
+						<p>{{ this.records.laugh }}</p>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="user-width notification user-div-style user-div-wrapper">
+		<div class="user-right-wrapper">
+			<div class="display-row user-div-style">
+				<div>
+					<p class="orange">name</p>
+					{{ this.user.name }}
+				</div>
+				<fa
+					:icon="faCog"
+					@click="changeComponent('UserSettings')"
+					class="user-icon cog"
+				/>
+			</div>
+			<div class="user-div-style display-row justify-between">
+				<p class="orange">video</p>
+				<div class="display-row justify-between video-icons">
+					<fa
+						:icon="faListUl"
+						@click="$store.dispatch('goToVideo', 'Video')"
+						class="user-icon"
+					/>
+					<fa
+						:icon="faPlus"
+						@click="$store.dispatch('goToVideo', 'VideoNew')"
+						class="user-icon"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="notification user-div-style">
 			<p class="orange">notification</p>
 			<template v-if="notifications">
 				<div class="display-row" v-for="item in notifications" :key="item.id">
-					<div class="notice">
+					<div class="notice-item">
 						<p>{{ item.message }}</p>
 					</div>
 					<span @click="noticeDelete(item.id)" class="notice-delete">×</span>
