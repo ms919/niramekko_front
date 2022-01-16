55988511bff95b8abcbf2d4a6fccbf61e3e6bcfe
ms919 <sakuraimiki919@gmail.com>
2022-01-16 20:01:11 +0900

    Add: 管理画面videoにcannot_play_flg絞り込み機能追加

--- components/admin/Videos.vue
+++ components/admin/Videos.vue
@@ -1,22 +1,28 @@
 <template>
   <div>
-    <CommonPagination :current-page="currentPage" :total-pages="totalPages" @getPage="getPage" class="pagination-wrapper"/>
-    <table>
-      <tr>
-        <th>id</th>
-        <th>user_id</th>
-        <th>top_flg</th>
-        <th>video</th>
-        <th>delete</th>
-      </tr>
-      <tr v-for="item in videoData" :key="item.id">
-        <td>{{item.id}}</td>
-        <td>{{item.user_id}}</td>
-        <td>{{item.latest_top_flg}}</td>
-        <td v-html="`<blockquote class='tiktok-embed item' cite='https://www.tiktok.com/@${item.video_user}/video/${item.data_video_id}' data-video-id=${item.data_video_id}><section></section></blockquote>`"></td>
-        <td><a @click="videoDelete(item.id)" class="btn xs-button btn-grey">delete</a></td>
-      </tr>
-    </table>
+    <template v-if="videoData.length > 0">
+      <CommonPagination :current-page="currentPage" :total-pages="totalPages" @getPage="getPage" class="pagination-wrapper"/>
+      <div class="checkbox"><input type="checkbox" id="cannot_play_flg"><label for="cannot_play_flg">cannot_play_flg</label></div>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>user_id</th>
+          <th>top_flg</th>
+          <th>video</th>
+          <th>delete</th>
+        </tr>
+        <tr v-for="item in videoData" :key="item.id">
+          <td>{{item.id}}</td>
+          <td>{{item.user_id}}</td>
+          <td>{{item.latest_top_flg}}</td>
+          <td v-html="`<blockquote class='tiktok-embed item' cite='https://www.tiktok.com/@${item.video_user}/video/${item.data_video_id}' data-video-id=${item.data_video_id}><section></section></blockquote>`"></td>
+          <td><a @click="videoDelete(item.id)" class="btn xs-button btn-grey">delete</a></td>
+        </tr>
+      </table>
+    </template>
+    <template v-else>
+      <p class="nothing-wrapper">該当のビデオがありません。</p>
+    </template>
   </div>
 </template>
 <script>
@@ -43,13 +49,15 @@ export default {
   },
   methods: {
     getPage(page){
+      const flg = document.getElementById('cannot_play_flg') != null ? document.getElementById('cannot_play_flg').checked : false;
       this.$axios
-			.get("api/v1/admin/videos", {params: { page: page }})
+			.get("api/v1/admin/videos", {params: { page: page, cannot_play_flg: flg }})
 			.then((res) => {
 				this.videoData = res.data.videos;
         this.totalPages = res.data.total_pages;
         this.currentPage = page;
         this.scriptFlg = !this.scriptFlg;
+        console.log(this.videoData.length > 0);
 			})
 			.catch(() => {
         this.showFlashMsg('error', 'アクセス権限がありません。');
@@ -83,5 +91,10 @@ export default {
 .pagination-wrapper {
   position: sticky;
   top: 0;
+}
+.checkbox {
+  text-align: left;
+  margin: 0.5rem;
+  font-size: 0.8rem;
 }
 </style>
